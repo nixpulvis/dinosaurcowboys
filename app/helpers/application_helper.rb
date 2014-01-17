187a55586839d76df5149b1560f0bd3745f407de
Nathan Lilienthal <nathan@nixpulvis.com>
2014-01-17 18:10:44 -0500

    Don't show updated in less than 1 min.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  # User -> String(URL)
+  # User -> HTML
   # <2 rbates @ railscasts.
   #
   def avatar_tag(user, options = {})
@@ -8,6 +8,14 @@ module ApplicationHelper
     source = "http://gravatar.com/avatar/#{gravatar_id}.png?s=50"
 
     image_tag(source, options)
+  end
+
+  # DateTime -> HTML
+  # Returns the needed HTML to have a live updated moment.js
+  # tag.
+  #
+  def from_now_tag(datetime)
+    content_tag(:abbr, datetime.iso8601, title: datetime, class: "datetime")
   end
 
 end
