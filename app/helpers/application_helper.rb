496728c090e26931aa205a8d9756bb5e95bbab2f
Nathan Lilienthal <nathan@nixpulvis.com>
2014-01-17 16:51:36 -0500

    Gravatar.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # User -> String(URL)
+  # <2 rbates @ railscasts.
+  #
+  def avatar_tag(user, options = {})
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    source = "http://gravatar.com/avatar/#{gravatar_id}.png?s=50"
+
+    image_tag(source, options)
+  end
+
 end
