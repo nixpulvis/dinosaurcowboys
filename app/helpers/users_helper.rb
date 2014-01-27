94217031f0938b140aead11efcfad0b38c9a211f
Nathan Lilienthal <nathan@nixpulvis.com>
2014-01-26 21:58:58 -0500

    Post pluralize fix.
    fixes #12

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   # Returns the formatted string for the users posts count.
   #
   def user_post_count(user)
-    pluralize(number_with_delimiter(user.posts.count), "posts")
+    pluralize(number_with_delimiter(user.posts.count), "post")
   end
 
 end
