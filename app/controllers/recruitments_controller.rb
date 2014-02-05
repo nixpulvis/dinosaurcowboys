1447f97a7f0dea368ef299079544f4e1c353e6f5
Nathan Lilienthal <nathan@nixpulvis.com>
2014-02-05 17:00:39 -0500

    Recruitment need partial.

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -4,16 +4,26 @@
 # Actions: [show, edit, update]
 #
 class RecruitmentsController < ApplicationController
+  # GET /recruitment
+  # Provides the recruitment class needs.
+  #
   def show
     @recruitments = policy_scope(RecruitmentClass)
     authorize @recruitments
   end
 
+  # GET /recruitment/edit
+  # Provides the recruitment class needs for editing.
+  #
   def edit
     @recruitments = policy_scope(RecruitmentClass)
     authorize RecruitmentClass
   end
 
+  # PATCH or PUT /recruitment
+  # Updates each of the recruitment classes in the params
+  # with their corresponding new values.
+  #
   def update
     authorize RecruitmentClass
 
