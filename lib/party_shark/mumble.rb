4f048c7aa2bf1869a8c8aeb59da9750fbee135a8
Nathan Lilienthal <nathan@nixpulvis.com>
2014-02-06 16:07:02 -0500

    lint

--- lib/party_shark/mumble.rb
+++ lib/party_shark/mumble.rb
@@ -1,12 +1,14 @@
 module PartyShark
-
+  # PartyShark::Mumble
+  # JSON API wrapper for typefrag's mumble api.
+  #
   class Mumble
     SERVER = 'partyshark.typefrag.com'
     PORT   = 7675
 
     include HTTParty
 
-    base_uri "http://www.typefrag.com/server-status/mumble/ChannelViewerProtocol.aspx"
+    base_uri 'http://www.typefrag.com/server-status/mumble/ChannelViewerProtocol.aspx'  # rubocop:disable LineLength
 
     attr_reader :data
 
@@ -19,13 +21,12 @@ module PartyShark
     class << self
       def data(server, port)
         params = {
-          "ReturnType" => "json",
-          "HostName"   => server,
-          "PortNumber" => port
+          'ReturnType' => 'json',
+          'HostName'   => server,
+          'PortNumber' => port
         }
-        get("/", query: params).parsed_response
+        get('/', query: params).parsed_response
       end
     end
   end
-
 end
