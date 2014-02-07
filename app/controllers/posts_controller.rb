9804f7b8ff438d118b0c7a791b5d252bebf726ea
Nathan Lilienthal <nathan@nixpulvis.com>
2014-02-07 13:49:30 -0500

    Fix page change when updating/destroying posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,31 +11,39 @@ class PostsController < ApplicationController
   end
 
   # POST /postable/:postable_id/posts
-  # Creates a post on the postable, as the current user.
+  # Creates a post on the postable, as the current user. Redirecting
+  # to the last page, because the new post should always be last.
   #
   def create
     @post = postable.posts.build(post_params)
     @post.user = current_user
     authorize @post
 
+    response_params = { page: last_page(postable) }
+
     @post.save
-    redirect_to postable_path(postable, page: last_page(postable))
+    redirect_to postable_path(postable, response_params)
   end
 
   # PATCH or PUT /postable/:postable_id/posts/:id
-  # Allows for users to update their posts.
+  # Allows for users to update their posts. Redirecting to the
+  # postable on the same page as it was on.
   #
   def update
+    response_params = { page: params[:page].blank? ? 1 : params[:page] }
+
     @post.update_attributes(post_params)
-    redirect_to postable_path(postable, page: last_page(postable))
+    redirect_to postable_path(postable, response_params)
   end
 
   # DELETE /postable/:postable_id/posts/:id
   # Destroys the post.
   #
   def destroy
+    response_params = { page: params[:page].blank? ? 1 : params[:page] }
+
     @post.destroy
-    redirect_to postable_path(postable)
+    redirect_to postable_path(postable, response_params)
   end
 
   private
