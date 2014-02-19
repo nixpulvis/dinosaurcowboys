c21a6d6425bacc66bef51bca125eb54f4c6abd0d
Nathan Lilienthal <nathan@nixpulvis.com>
2014-02-19 11:05:10 -0500

    Fix deprication warnings for I18n.

--- config/application.rb
+++ config/application.rb
@@ -10,17 +10,10 @@ module PartyShark
   class Application < Rails::Application
     require 'party_shark'
 
+    # Handle errors internally.
     config.exceptions_app = self.routes
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # I'm not localizing this application.
+    config.i18n.enforce_available_locales = false
   end
 end
