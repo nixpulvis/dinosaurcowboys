d9c2b09cf67cff2f49e5c72590ce6b6e1ad94730
Nathan Lilienthal <nixpulvis@users.noreply.github.com>
2014-03-27 21:25:28 -0400

    ActionController::InvalidAuthenticityToken fix.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   # Allowing things like iframes in content of the site requires this.
   # The browser by default shits itself if a response comes back with
