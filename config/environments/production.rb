10da8b645e11c1eda6e3dade82d992fb724ec04b
Nathan Lilienthal <nathan@nixpulvis.com>
2014-05-12 19:04:54 -0400

    Attempt to fix New Relics bullshit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,10 +14,14 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
+  # Fuck you New Relic, why are you breaking ETag generation.
+  # http://stackoverflow.com/questions/18453435/newrelic-causing-newer-etags-to-be-generated-on-each-request
+  if defined? NewRelic::Rack::BrowserMonitoring
+    config.after_initialize do
+      config.middleware.delete "Rack::ETag"
+      config.middleware.insert_after "NewRelic::Rack::BrowserMonitoring", "Rack::ETag"
+    end
+  end
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
