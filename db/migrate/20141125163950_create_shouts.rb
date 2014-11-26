1aba184a1d5f1fc5b16bf8ed0c3c5a53b1fbb692
Anthony Barone <tonybaroneee@gmail.com>
2014-11-25 19:24:43 -0500

    Adjustments based on code review and RuboCop

--- db/migrate/20141125163950_create_shouts.rb
+++ db/migrate/20141125163950_create_shouts.rb
@@ -1,7 +1,7 @@
 class CreateShouts < ActiveRecord::Migration
   def up
     create_table :shouts do |t|
-      t.string      :shout,   null: false
+      t.string      :message, null: false
       t.boolean     :hidden,  default: false
       t.belongs_to  :user
 
