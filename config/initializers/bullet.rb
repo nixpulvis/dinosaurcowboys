af209afc09d5c137ccdcaad7d0717f2c105fa247
Nathan Lilienthal <nathan@nixpulvis.com>
2015-01-10 22:43:18 -0800

    SCSS, working.

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -4,6 +4,6 @@ if Rails.env.development?
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.rails_logger = true
-    Bullet.add_footer = true
+    Bullet.add_footer = false
   end
 end
