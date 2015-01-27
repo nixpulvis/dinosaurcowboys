03c2381f99f77dc5da54a4cc3c9ad61704c983de
Nathan Lilienthal <nathan@nixpulvis.com>
2015-01-26 22:34:56 -0800

    Config cleanup, remove old tokens.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,78 +1,21 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # Code is not reloaded between requests.
   config.cache_classes = true
-
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
-
-  # Fuck you New Relic, why are you breaking ETag generation.
-  # http://stackoverflow.com/questions/18453435/newrelic-causing-newer-etags-to-be-generated-on-each-request
-  if defined? NewRelic::Rack::BrowserMonitoring
-    config.after_initialize do
-      config.middleware.delete "Rack::ETag"
-      config.middleware.insert_after "NewRelic::Rack::BrowserMonitoring", "Rack::ETag"
-    end
-  end
-
-  # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = false
-
-  # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
-  # Generate digests for assets URLs.
   config.assets.digest = true
-
-  # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
-  # Set to :debug to see everything in the log.
   config.log_level = :info
-
-  # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Settup mailer in production.
   config.action_mailer.default_url_options = { host: 'www.partyshark.org' }
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
+  config.lograge.enabled = true
+  config.log_formatter = ::Logger::Formatter.new
+  config.active_record.dump_schema_after_migration = false
 
-  # Setup SendGrid.
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
@@ -83,7 +26,6 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
-  # Store attachments on S3.
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
@@ -93,22 +35,10 @@ Rails.application.configure do
     }
   }
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
-
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
-
-  # Control the logging.
-  config.lograge.enabled = true
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  if defined? NewRelic::Rack::BrowserMonitoring
+    config.after_initialize do
+      config.middleware.delete "Rack::ETag"
+      config.middleware.insert_after "NewRelic::Rack::BrowserMonitoring", "Rack::ETag"
+    end
+  end
 end
--- config/initializers/exceptions.rb
+++ config/initializers/exceptions.rb
@@ -1,6 +1,3 @@
-# Append expections we want to route to ActionDispatch's
-# ExceptionWrapper.
-#
 module ActionDispatch
   class ExceptionWrapper
     @@rescue_responses.merge!('Pundit::NotAuthorizedError' => :forbidden)
