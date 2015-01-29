b539727d2f588a73afbf6b55e39cfd0148ddd600
Nathan Lilienthal <nathan@nixpulvis.com>
2015-01-29 13:47:34 -0800

    Forgot to change rollbar to use secrets.

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   require 'rollbar/rails'
   Rollbar.configure do |config|
-    config.access_token = 'aac7434c543b49c587584dc03a293182'
+    config.access_token = Rails.application.secrets.rollbar_access_token
   end
 end
