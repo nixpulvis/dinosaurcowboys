be658dcc63c1383a38cfe0c058a70cd817e32d05
Nathan Lilienthal <nathan@nixpulvis.com>
2015-02-06 11:00:30 -0800

    rack attack

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ module PartyShark
   class Application < Rails::Application
     require 'party_shark'
 
+    config.middleware.use(Rack::Attack)
+
+    Rack::Attack.blacklist('block 98.238.22.33') do |req|
+      '98.238.22.33' == req.ip
+    end
+
     # Redirect requests to partyshark.herokuapp.com to our domain.
     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
       r301 %r{.*}, 'http://www.partyshark.org$&', :if => Proc.new { |env|
